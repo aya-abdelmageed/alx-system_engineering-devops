b64ecfc7dd35793d2693368dfa0d7e3a5c1e0b52
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2023-09-27 15:54:48 -0700

    last fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,14 @@
-#!/usr/bin/env bash
 # using Puppet to make changes to our configuration file
 
-file { 'ect/ssh/ssh_config':
-	ensure => present,
+file_line { 'Turn off password auth':
+  ensure => 'present',
+    path => '/etc/ssh/ssh_config',
+    line => '    PasswordAuthentication no',
+  match  => '#     PasswordAuthentication yes',
+}
 
-content =>"
-	#SSH client configuration
-	host*
-	IdentityFile ~/.ssh/school
-	PasswordAuthentication no
-	",
+file_line { 'Declare identity file':
+  ensure => 'present',
+    path => '/etc/ssh/ssh_config',
+    line => '    IdentityFile ~/.ssh/holberton',
 }
